3fdab8ea3d68fefd34cd02feb3e171abb0a42a18
Vusal Layijov <Layijovvusal@gmail.com>
2023-01-19 12:52:30 -0800

    Initialize Express and Sequelize with CSRF protection

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+const app = express();
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+
+const routes = require('./routes');
+
+
+if (!isProduction) {
+    // enable cors only in development
+    app.use(cors());
+}
+app.use(
+    helmet.crossOriginResourcePolicy({
+        policy: "cross-origin"
+    })
+);
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
+    })
+);
+app.use(routes);
+
+module.exports = app;
