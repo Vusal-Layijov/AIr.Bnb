858a0fc506d72995a9129bad97afba2a706eb6d9
Vusal Layijov <Layijovvusal@gmail.com>
2023-01-25 16:43:46 -0800

    Add image to review based on Reviews id

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,7 @@ const { User } = require('../../db/models');
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js')
+const reviewsRouter = require('./reviews.js')
 const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
 
@@ -14,7 +15,9 @@ router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
-router.use('/spots', spotsRouter)
+router.use('/spots', spotsRouter);
+
+router.use('/reviews',reviewsRouter)
 // router.get(
 //     '/restore-user',
 //     (req, res) => {
