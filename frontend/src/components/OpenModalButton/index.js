20d025ef3342e3db9e8bc116a5818ea05b520324
Vusal Layijov <Layijovvusal@gmail.com>
2023-04-26 14:43:54 -0700

    new features added

--- frontend/src/components/OpenModalButton/index.js
+++ frontend/src/components/OpenModalButton/index.js
@@ -16,7 +16,7 @@ function OpenModalButton({
     };
 
     return (
-        <button onClick={onClick}>{buttonText}</button>
+        <button className='forButStyle' onClick={onClick}>{buttonText}</button>
     );
 }
 
