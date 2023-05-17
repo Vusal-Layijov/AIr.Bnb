05be8181dde6b0d165347e9558ec8f82edcffbae
Vusal Layijov <Layijovvusal@gmail.com>
2023-05-16 19:38:39 -0700

    changes to gallery

--- frontend/src/components/SetOneSpot/ImageGallery.js
+++ frontend/src/components/SetOneSpot/ImageGallery.js
@@ -1,3 +1,13 @@
-export default function ImageGallery(){
-    
+export default function ImageGallery({images}){
+    return (
+        <div className="single-spot-gallery" >
+            {images.map(img=> (
+                <div key={img.id} className={img.preview ? "main-tile":"small-tile"}>
+                    <img className="gallery-image-div" src={img.url} />
+                </div>
+            ))
+
+            }
+        </div>
+    )
 }
