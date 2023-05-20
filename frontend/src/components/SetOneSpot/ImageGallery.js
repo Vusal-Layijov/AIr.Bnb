d3c71ee606578d8e70afd7a6739ae46142e6f586
Vusal Layijov <Layijovvusal@gmail.com>
2023-05-19 19:52:06 -0700

    changes toimage  gallery

--- frontend/src/components/SetOneSpot/ImageGallery.js
+++ frontend/src/components/SetOneSpot/ImageGallery.js
@@ -1,6 +1,9 @@
-export default function ImageGallery({images}){
+import SpotsMap from "../Map"
+import './SetOneSpot.css'
+export default function ImageGallery({images,spots}){
     return (
         <div className="single-spot-gallery" >
+            <>
             {images.map(img=> (
                 <div key={img.id} className={img.preview ? "main-tile":"small-tile"}>
                     <img className="gallery-image-div" src={img.url} />
@@ -8,6 +11,10 @@ export default function ImageGallery({images}){
             ))
 
             }
+            <div className="second-tile" >
+                <SpotsMap spots={spots} />
+            </div>
+            </>
         </div>
     )
 }
