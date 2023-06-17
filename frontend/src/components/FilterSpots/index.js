79f89b12c3f1e8d83e10a4aafc05201c732ed6a3
Vusal Layijov <Layijovvusal@gmail.com>
2023-06-16 23:58:50 -0700

    filter search compenent

--- frontend/src/components/FilterSpots/index.js
+++ frontend/src/components/FilterSpots/index.js
@@ -1,4 +1,13 @@
+import { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { NavLink, useParams } from "react-router-dom"
+import { useSearchParams } from "../../context/search"
+
+
+
 export default function FilterSpots(){
+    const { searchParams, setSearchParams } = useSearchParams('')
+    // console.log('------.-> search paramsssss',searchParams)
     return(
         <h1>Search features are coming</h1>
     )
