614d6c64c31312351636a272d02d7aa46896b3c5
Vusal Layijov <Layijovvusal@gmail.com>
2023-06-18 22:57:40 -0700

    filtered spot componnent

--- frontend/src/Image/Image.js
+++ frontend/src/Image/Image.js
@@ -2,7 +2,5 @@ import React from 'react'
 import logo from './IMG_5926.jpeg'
 import './image.css'
 export default function Image() {
-    console.log('rendering image')
- 
   return <img src={logo} alt='logo' className='logo' />
 }
---
+++ frontend/src/components/FilterSpots/FilteredSpots.js
@@ -0,0 +1,5 @@
+import { setSpotsWithQuery } from "../../store/spots"
+
+export default function setFilteredSpots(){
+
+}
